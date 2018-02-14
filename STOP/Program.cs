8e1b51b4c31c0c4a7ad67d204d31d545c6bad90e
iliaanastasov <36041694+iliaanastasov@users.noreply.github.com>
2018-02-14 16:09:08 +0200

    Add files via upload

---
+++ STOP/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace STOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine("{0}{1}{0}", new string('.', n + 1),
+                new string('_', 2 * n + 1));
+
+            for (int i = n; i > 0; i--)
+            {
+                Console.WriteLine("{0}//{1}\\\\{0}", new string('.', i),
+                new string('_', 4 * n - 1 - 2 * i)
+                    );
+
+            }
+            Console.WriteLine("//{0}STOP!{0}\\\\", new string('_', (4 * n - 6) / 2));
+            for (int j = 0; j < n; j++)
+            {
+                Console.WriteLine("{0}\\\\{1}//{0}", new string('.', j),
+                new string('_', 4 * n - 1 - 2 * j)
+                    );
+
+
+            }
+        }
+    }
+}
